5628c139e8e9192e0509c7be792edf6197821489
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2026-06-03 16:22:47 +0200

    Add exercise to Day 2 sheet with its solution

---
+++ Bash/Exercise_Sheet_2/Exercise_7.bash
@@ -0,0 +1,28 @@
+#!/usr/bin/env BASH
+
+printf '\nInsert words (s|show to show count, r|reset to reset count, q|quit to exit):\n'
+
+declare -A counter
+
+while read -r -p " > " word; do
+    words=( ${word} ) # Let word splitting split
+    for w in "${words[@]}"; do
+        case "${w}" in
+            s | show )
+                for key in "${!counter[@]}"; do
+                    printf "%30s: %d\n" "${key}" "${counter[${key}]}"
+                done
+                ;;
+            r | reset )
+                counter=()
+                ;;
+            q | quit )
+                printf '\n'
+                exit 0
+                ;;
+            *)
+                counter["${w,,}"]=$(( counter["${w,,}"] + 1 ))
+                ;;
+        esac
+    done
+done
